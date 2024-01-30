64602e64c4c0833dca78983bafa7c41936fc84c1
Akintobi Moyinoluwa Ezekiel <akintobi41moyinoluwa@gmail.com>
2024-01-30 14:07:25 +0100

    Created Login componet

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,31 @@
+import authService from "./../../appwrite/auth";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Button from "./../../components/button/Button";
+import Input from "./../../components/input/Input";
+import Logo from "./../../components/logo/Logo";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { login as authLogin } from "../../store/authSlice";
+
 function Login() {
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+
+  const login = async (data) => {
+    setError("");
+    try {
+      const session = await authService.getCurrentUser();
+
+      if (session) {
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return <div>Login</div>;
 }
 
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,11 +1,12 @@
-import authService from "./../../appwrite/auth";
-import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../../store/authSlice";
+import authService from "./../../appwrite/auth";
 import Button from "./../../components/button/Button";
 import Input from "./../../components/input/Input";
 import Logo from "./../../components/logo/Logo";
-import { useForm } from "react-hook-form";
-import { useDispatch } from "react-redux";
 
 function Signup() {
   const navigate = useNavigate();
@@ -18,12 +19,71 @@ function Signup() {
 
     try {
       const userData = await authService.createAccount(data);
+      if (userData) {
+        const userData = await authService.getCurrentUser();
+        if (userData) dispatch(login({ userData }));
+        navigate("/");
+      }
     } catch (error) {
       setError(error.message);
     }
   };
 
-  return <div>Signup</div>;
+  // and it was called userData and the create Account method was used
+  // also we tried to get the current user and then it was dispatched to the store (Redux Store) and then we navigated the user back to the homepage
+
+  return (
+    <div className="flex items-center justify-center">
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+      >
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px]">
+            <Logo width="100%" />
+          </span>
+        </div>
+        <h2 className="text-center text-2xl font-bold leading-tight">
+          Sign up to create account
+        </h2>
+        <p className="mt-2 text-center text-base text-black/60">
+          Already have an account?&nbsp;
+          <Link
+            to="/login"
+            className="font-medium text-primary transition-all duration-200 hover:underline"
+          >
+            Sign In
+          </Link>
+        </p>
+        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+        <form onSubmit={handleSubmit(create)} className="mt-8">
+          <div className="space-y-5">
+            <Input
+              {...register("name", { required: true })}
+              label="Full Name : "
+              placeholder="Full Name"
+            />
+            <Input
+              {...register("email", {
+                required: true,
+              })}
+              label="Email : "
+              placeholder="Email Address"
+              type="email"
+            />
+            <Input
+              {...register("password", { required: true })}
+              label="Password : "
+              type="password"
+              placeholder="Password"
+            />
+            <Button type="submit" className="w-full">
+              Create Account
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Signup;
